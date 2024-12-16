c7db61195f2798e423bb927bf496e0c6833d82e1
Luca Bruno <lucabruno123@gmail.com>
2024-12-16 18:49:12 -0300

    Agregando estilos

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -42,27 +42,39 @@ function ToDoList() {
     }
   }
   return (
-    <div className="flex flex-col items-center border border-black">
-      <h1 className="uppercase py-5 font-bebas text-7xl">To-Do List</h1>
+    <div className="flex flex-col items-center bg-black w-full h-screen border border-white">
+      <h1 className="uppercase py-5 font-bebas text-7xl text-white">
+        To-Do List
+      </h1>
 
-      <div>
+      <div className="flex w-1/4 justify-center gap-14">
         <input
           type="text"
           placeholder="Enter a task..."
           value={newtask}
           onChange={handleInputChange}
-          className="border border-black"
+          className="p-3 text-xl"
         />
-        <button onClick={addTask}>Add</button>
+        <button
+          onClick={addTask}
+          className="text-white text-xl border border-white px-6"
+        >
+          Add
+        </button>
       </div>
 
-      <ol>
+      <ol className="w-1/3 my-10">
         {tasks.map((task, index) => (
-          <li key={index}>
+          <li
+            key={index}
+            className="text-white text-xl border border-white flex justify-between p-5"
+          >
             <span>{task}</span>
-            <button onClick={() => deleteTask(index)}>Delete</button>
-            <button onClick={() => moveTaskUp(index)}>Up</button>
-            <button onClick={() => moveTaskDown(index)}>Down</button>
+            <div>
+              <button onClick={() => deleteTask(index)}>Delete</button>
+              <button onClick={() => moveTaskUp(index)}>Up</button>
+              <button onClick={() => moveTaskDown(index)}>Down</button>
+            </div>
           </li>
         ))}
       </ol>
